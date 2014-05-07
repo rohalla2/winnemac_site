21c4bc312d72ed5e4349eb5a7344eb8190723b2c
Jeff Rohalla <jrohalla@uchicago.edu>
2014-05-06 23:35:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WinnemacSite::Application.config.secret_key_base = '30ac0c07bda2fbe3ff995c0d4463a79311bb45c5527dad6f41dab9ea18e6310ea1235bef55362aefdc8b7d44685fa1c368aa3b97abbf8d895b825c3e97319550'
