b84e1377152309a9f85044c6d9a6b35b5af64936
Jeff Rohalla <jeffrohalla@gmail.com>
2015-07-19 22:07:43 -0500

    Add contact mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,8 @@
+class ContactMailer < ActionMailer::Base
+  default from: "contact@2433winnemac.com"
+
+  def contact_email(contact_params)
+    @message_info = contact_params
+    mail(to: ENV["CONTACT_EMAIL"], subject: 'Contact Submission for 2433winnemac.com'))
+  end
+end
