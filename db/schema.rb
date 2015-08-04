412d9289cbc6ea99eb58c06d22820721e26ae2ce
Jeff Rohalla <jeffrohalla@gmail.com>
2015-08-03 23:10:52 -0500

    Drop messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803052405) do
+ActiveRecord::Schema.define(version: 20150804040950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,17 +34,6 @@ ActiveRecord::Schema.define(version: 20150803052405) do
     t.datetime "updated_at"
     t.string   "description"
     t.integer  "unit_id"
-  end
-
-  create_table "messages", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "subject"
-    t.string   "message"
-    t.boolean  "read"
-    t.string   "referred_from"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "spaces", force: true do |t|
